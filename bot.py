e508a638864295d0ec29351b08478d2d5bed48bb
Egor Perevoshchikov <egorka7999@mail.ru>
2022-12-07 22:44:55 +0300

    Добавлено: возможность определять расписание на сегодняшний день; поддержка ответа, если день недели - воскресенье
    Подробнее:
    
    I. Модуль schedule.py
    	1) Для реализации возможности определния расписания на сегодняшний день была реализована функция get_today_day_ind(weekday), определяющая индекс текущего дня недели
    	2) Добавлен ответ, если индекс требуемого дня равен 6 (соотв. воскресенью). Ответ: "Воу, в воскресенье отдыхать надо, дружище"
    
    Полный функционал:
    1)	Вывод расписания по требуемому дню недели
    
    Стилевое оформление:
    1)	1. В начале сообщение выводится день недели прописными буквами
    	2. Формат записи - время начала и конца пары, название предмета, ФИО преподавателя, номер аудитории

--- bot.py
+++ bot.py
@@ -5,18 +5,18 @@ bot = telebot.TeleBot(token)
 
 filename = 'S-Б21-505.txt'
 
-def getsch(day_of_week):
+def getsch(weekday):
 	try:
-		return schedule.get_schedule(filename, day_of_week)
+		return schedule.get_schedule(filename, weekday)
 	except Exception as e:
-		return 'Какая-то неполадка :(' + str(e) + ')' 
+		return str(e)
 	
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
 	bot.send_message(m.chat.id, 'Привет, я показываю расписание')
 
 @bot.message_handler(content_types=["text"])
-def handle_text(message):
+def handle_weekday(message):
 	bot.send_message(message.chat.id, getsch(message.text), parse_mode="Markdown")
 
 bot.polling(none_stop=True, interval=0)
--- schedule.py
+++ schedule.py
@@ -1,14 +1,14 @@
-import fitz, re, fmtpdf, style
+import fitz, re, fmtpdf, style, time
 
 
-shorts = {0: ['пн', 'по'], 1: ['вт'], 2: ['ср'], 3: ['чт', 'че'], 4: ['пт', 'пя']}
+shorts = {0: ['пн', 'по'], 1: ['вт'], 2: ['ср'], 3: ['чт', 'че'], 4: ['пт', 'пя'], 5: ['су'], 6: ['вс', 'во']}
 data_path = 'C:/Python/scripts/tbot/data/'
 
 
-def get_schedule(filename_txt, day_of_week):
+def get_schedule(filename_txt, weekday):
 	text = fmtpdf.load_from_txt(filename_txt)
 	days = split_by_days(text)
-	day = sch_by_dow(days, day_of_week)
+	day = sch_by_dow(days, weekday)
 	day = modify_day(day)
 
 	return day
@@ -28,27 +28,39 @@ def split_by_days(text):
 	text = re.sub('\nСРЕДА', '\n\n\nСРЕДА\n', text)
 	text = re.sub('\nЧЕТВЕРГ', '\n\n\nЧЕТВЕРГ\n', text)
 	text = re.sub('\nПЯТНИЦА', '\n\n\nПЯТНИЦА\n', text)
+	text = re.sub('\nСУББОТА', '\n\n\nСУББОТА\n', text)
 
 	days = text.split('\n\n\n')
 
 	return days
 
 
-def sch_by_dow(days, day_of_week):
-	day_ind = get_day_ind(day_of_week)
+def sch_by_dow(days, weekday):
+	day_ind = get_day_ind(weekday)
 
 	if day_ind == -1: 
-		return 'Неверный ввод дня недели'
+		raise Exception('Неверный ввод дня недели')
+
+	if day_ind == 6:
+		raise Exception('Воу, в воскресенье отдыхать надо, дружище')
 
 	return days[day_ind].strip()
 
 
-def get_day_ind(day_of_week):
-	day_of_week = day_of_week.lower()
+def get_day_ind(weekday):
+	weekday = weekday.lower()
 
 	for x in shorts:
 		for y in shorts[x]:
-			if re.match(y + '[а-я]*', day_of_week):
+			if re.match(y + '[а-я]*', weekday):
 				return x
 
+	if re.match('се[а-я]*', weekday):
+		return get_today_day_ind(weekday)
+
 	return -1
+
+
+def get_today_day_ind(weekday):
+	now = time.localtime()
+	return now.tm_wday
